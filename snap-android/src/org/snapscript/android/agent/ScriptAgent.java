647e88bcc408970f335cd120c9e16b31ac15bc53
Niall Gallagher <gallagher_niall@yahoo.com>
2017-01-22 03:33:11 +0000

    Update android activity

--- snap-android/src/org/snapscript/android/agent/ScriptAgent.java
+++ snap-android/src/org/snapscript/android/agent/ScriptAgent.java
@@ -10,8 +10,11 @@ import android.os.Bundle;
 public class ScriptAgent extends Activity {
 
    public static final String REMOTE_HOST = "192.168.1.66";
+   public static final String LOG_LEVEL = "DEBUG";
    public static final int HTTP_PORT = 4457;
    public static final int EVENT_PORT = 4456;
+   public static final int THREAD_COUNT = 4;
+   public static final int STACK_SIZE = 2000000;
    
    @Override
    public void onCreate(Bundle bundle) {
@@ -20,8 +23,11 @@ public class ScriptAgent extends Activity {
          ProcessAgent agent = new ProcessAgent(
                URI.create("http://"+REMOTE_HOST+":"+HTTP_PORT+"/resource"), 
                "android-" + System.currentTimeMillis(), 
-               EVENT_PORT);
-         agent.run();
+               LOG_LEVEL,
+               EVENT_PORT, 
+               THREAD_COUNT,
+               STACK_SIZE);
+         agent.start();
       } catch (Exception e) {
          e.printStackTrace();
       }
